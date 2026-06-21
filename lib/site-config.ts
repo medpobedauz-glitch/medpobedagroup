3e90c494fd609aaab12d91425d8097d87d97f260
guptaravishankar55-tech <guptaravishankar55@gmail.com>
2026-06-21 19:00:51 +0500

    feat: add BlogAiRun tracking model, instagram link, regen sitemap

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -10,7 +10,7 @@ export const siteConfig = {
   location: "Tashkent, Uzbekistan",
   website: "medpobedagroup.uz",
   socialLinks: {
-    instagram: "",
+    instagram: "https://www.instagram.com/medpobedagroup/",
     facebook: "",
     linkedin: "",
     telegram: "https://t.me/998910124043",
